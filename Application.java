b464821e6ef0e3f058af557d3a933bc12838c6ec
Paul Friedli <paul.friedli@edufr.ch>
2026-08-28 10:11:00 +0200

    Initial commit

---
+++ Application.java
@@ -0,0 +1,20 @@
+public class Application {
+
+    // -----------------------------------------------------------
+    // Sujets : Déclaration variable, output console, vérifier CodeExam sous charge
+    // -----------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        System.out.println("Good morning World !");
+
+        // Déclarez une variable contenant votre nom
+
+        // Déclarez une variable contenant votre âge
+
+        // Affichez votre nom et votre âge sous cette forme
+        // "Je m'appelle NOM Prénom et j'ai 17 ans"
+
+    }
+
+}
